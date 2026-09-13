ba993a3afac6e2d66732db3ab09a3eca05e92576
kevin kalenga <kevinkalenga10@gmail.com>
2026-09-13 23:53:21 +0200

    Search Room by Location

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,12 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
-import Room from "../models/room"
+import Room, { IRoom } from "../models/room"
 import ErrorHandler from "../utils/errorHandler";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
+import APIFilters from "../utils/apiFilters";
 
 // Get all rooms => /api/rooms
 export const allRooms =  catchAsyncErrors(async (req: NextRequest) => {
    const resPerPage: number = 8
-   const rooms = await Room.find();
+  //const rooms = await Room.find();
+   //get the value for the url
+   const {searchParams} = new URL(req.url)
+
+   //console.log(searchParams)
+
+
+  //  query string
+   const queryStr: any = {}
+
+   searchParams.forEach((value, key) => {
+      queryStr[key] = value
+   })
+
+  //console.log(queryStr)
+
+   const apiFilters = new APIFilters(Room, queryStr).search()
+
+   const rooms: IRoom[] = await apiFilters.query
 
    return NextResponse.json({
      success: true,
