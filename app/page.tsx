7f7fbe9e81b63aa10da361149300c5689f380d70
kevin kalenga <kevinkalenga10@gmail.com>
2026-09-17 00:14:14 +0200

    Data Caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Home from "@/components/Home";
 
 const getRooms = async () => {
-  const res = await fetch("http://localhost:3000/api/rooms");
+  const res = await fetch("http://localhost:3000/api/rooms", {
+    //This will not cache the data 
+    cache:"no-store"
+  });
   return res.json()
 }
 
